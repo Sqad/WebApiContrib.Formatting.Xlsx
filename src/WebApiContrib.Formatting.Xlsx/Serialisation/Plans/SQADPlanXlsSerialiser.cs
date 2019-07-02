65a893854e2ee8ac680ff3276d9597ee10881d8d
Stepan Kobzey <stepankobzey@gmail.com>
2019-07-02 09:44:16 -0500

    fix cleanup and fic for proper value getter

--- src/WebApiContrib.Formatting.Xlsx/Serialisation/Plans/SQADPlanXlsSerialiser.cs
+++ src/WebApiContrib.Formatting.Xlsx/Serialisation/Plans/SQADPlanXlsSerialiser.cs
@@ -441,7 +441,7 @@ namespace SQAD.MTNext.WebApiContrib.Formatting.Xlsx.Serialisation.Plans
 
                             ExcelCell valuePreservationCell = new ExcelCell();
                             valuePreservationCell.CellHeader = $"{columnNameCombined}:Value:{objID}";
-                            if (customFieldItem != null && customFieldItem.GetType().GetProperty("ComputedValue") == null)
+                            if (customFieldItem != null)
                             {
                                 valuePreservationCell.CellValue = customFieldItem.Value;
                                 customValueHeaderCell.CellValue = customFieldItem.Value;
@@ -451,13 +451,9 @@ namespace SQAD.MTNext.WebApiContrib.Formatting.Xlsx.Serialisation.Plans
                                     customValueHeaderCell.CellValue = customFieldItem.Override;
                                 }
                             }
-                            else
-                            {
-                                valuePreservationCell.CellValue = customFieldItem.ComputedValue;
-                                customValueHeaderCell.CellValue = customFieldItem.ComputedValue;
-                            }
 
-                            if (valuePreservationCell.CellValue!=null && valuePreservationCell.CellValue.GetType() == typeof(DateTime))
+
+                            if (valuePreservationCell.CellValue != null && valuePreservationCell.CellValue.GetType() == typeof(DateTime))
                             {
                                 valuePreservationCell.CellValue = valuePreservationCell.CellValue.ToString();
                                 customValueHeaderCell.CellValue = customValueHeaderCell.CellValue.ToString();
@@ -472,7 +468,7 @@ namespace SQAD.MTNext.WebApiContrib.Formatting.Xlsx.Serialisation.Plans
 
                             ExcelCell textPreservationCell = new ExcelCell();
                             textPreservationCell.CellHeader = $"{columnNameCombined}:Text:{objID}";
-                            textPreservationCell.CellValue = customFieldItem.Override;
+                            textPreservationCell.CellValue = customFieldItem.Text;
                             CreatePreserveCell(textPreservationCell, document);
 
                             ExcelCell hiddenTextPreservationCell = new ExcelCell();

e2638f49f8f050471bde5e12b751fbe5a9cc203f
gbalot <gbalotskiy@sqad.com>
2019-09-05 14:11:19 -0500

    initial commit

---
+++ src/WebApiContrib.Formatting.Xlsx/Serialisation/Actuals/SQADActualXlsSerialiser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQAD.MTNext.WebApiContrib.Formatting.Xlsx.Serialisation.Actuals
+{
+    public class SQADActualXlsSerialiser
+    {
+    }
+}
